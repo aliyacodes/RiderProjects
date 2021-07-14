c294194963c96ec5487c7aeb5b969f04e1907156
aliyacodes <aasken01@seattlecentral.edu>
2021-07-14 16:47:29 -0700

    math methods

--- Giraffe/Giraffe/Program.cs
+++ Giraffe/Giraffe/Program.cs
@@ -6,9 +6,14 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            int num = 6;
-            num++;
-            Console.WriteLine(num);
+          
+            Console.WriteLine( Math.Abs(-40) ); //math methods
+            Console.WriteLine( Math.Pow(3.8, 2) );
+            Console.WriteLine( Math.Sqrt(36) );
+            Console.WriteLine( Math.Max(4, 90) );
+            Console.WriteLine( Math.Min(4, 90) );
+            Console.WriteLine( Math.Round(4.3) );
+            Console.WriteLine( Math.Round(4.6) );
             
             Console.ReadLine(); 
         }
