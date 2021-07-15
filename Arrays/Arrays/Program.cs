e9aedf45736f5c4f912030e2bf946797a932b4f6
aliyacodes <aasken01@seattlecentral.edu>
2021-07-15 13:42:40 -0700

    Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MadLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = {4, 8, 15, 16, 23, 42}; //<-- elements inside of array 
+            
+            Console.WriteLine(luckyNumbers[0]);
+            
+            Console.ReadLine();
+        }
+    }
+}
