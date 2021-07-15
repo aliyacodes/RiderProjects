5def933ddafbecf6fbdff7b374528893f4a3f0af
aliyacodes <aasken01@seattlecentral.edu>
2021-07-14 18:40:34 -0700

    getting user input

--- Giraffe/Giraffe/Program.cs
+++ Giraffe/Giraffe/Program.cs
@@ -6,14 +6,11 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-          
-            Console.WriteLine( Math.Abs(-40) ); //math methods
-            Console.WriteLine( Math.Pow(3.8, 2) );
-            Console.WriteLine( Math.Sqrt(36) );
-            Console.WriteLine( Math.Max(4, 90) );
-            Console.WriteLine( Math.Min(4, 90) );
-            Console.WriteLine( Math.Round(4.3) );
-            Console.WriteLine( Math.Round(4.6) );
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age);
             
             Console.ReadLine(); 
         }
