0d4a03fb09e2f1fc90a7156b3b24a33d16a01dd0
aliyacodes <aasken01@seattlecentral.edu>
2021-07-15 15:18:40 -0700

    Return Statements

---
+++ ReturnStatements/ReturnStatements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReturnStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+            
+            Console.ReadLine();
+        }
+
+        static int cube(int num) // <--specify parameter to return num cubed
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
