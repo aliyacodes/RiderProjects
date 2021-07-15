d18f0e9dd035501ec3dda3aeffc722ed9074762e
aliyacodes <aasken01@seattlecentral.edu>
2021-07-15 13:47:59 -0700

    arrays2

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -7,8 +7,13 @@ namespace MadLibs
         static void Main(string[] args)
         {
             int[] luckyNumbers = {4, 8, 15, 16, 23, 42}; //<-- elements inside of array 
+            string[] friends = new string[5]; //friends array can hold 5 values
+            friends[0] = "Jim"; //individually populate element in array
+            friends[1] = "Kelly";
             
-            Console.WriteLine(luckyNumbers[0]);
+            luckyNumbers[1] = 900;
+            
+            Console.WriteLine(luckyNumbers[1]);
             
             Console.ReadLine();
         }
